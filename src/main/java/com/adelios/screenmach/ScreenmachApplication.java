f1ce50336977db02c021354bb0acb59006fb4494
delio404 <adelio.a.castro@gmail.com>
2023-07-19 23:09:40 -0300

    first commit

---
+++ src/main/java/com/adelios/screenmach/ScreenmachApplication.java
@@ -0,0 +1,13 @@
+package com.adelios.screenmach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmachApplication.class, args);
+    }
+
+}
